5955fa648fcb255fead0af6dc8188442e84145d2
seodowa <pagalan.theodoreustp@gmail.com>
2025-07-20 16:48:31 +0800

    Add functionality to some buttons
    Now when clicking the number (and dot) buttons, it displays
    the corresponding value on screen. The operator buttons don't work
    yet, but the CE button now works.

--- logic.js
+++ logic.js
@@ -1,4 +1,28 @@
+let buttonsContainer = document.querySelector(".input-buttons-container");
+let clearBtn = document.querySelector("#clear-empty");
+let inputScreen = document.querySelector("input");
 
+buttonsContainer.addEventListener("click", isValidInputForNumber);
+clearBtn.addEventListener("click", clearInputScreen);
+window.addEventListener("beforeunload", clearInputScreen);
+
+
+function clearInputScreen() {
+    inputScreen.value = "";
+}
+
+
+function isValidInputForNumber(event) {
+    let target = event.target;
+    
+    const re = RegExp("[\\d.]+");
+    let result;
+    
+    if ((result = re.exec(target.textContent)) === null)
+        return;
+
+    inputScreen.value += result[0];
+}
 
 
 function add(a, b) {
