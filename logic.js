251e4f8dcc0e47d0c8a33802ca70488c10cd7368
seodowa <pagalan.theodoreustp@gmail.com>
2025-07-20 23:15:47 +0800

    Enable input screen to scroll horizontally

--- logic.js
+++ logic.js
@@ -11,20 +11,23 @@ let num1 = num2 = result = null;
 let operation;
 let isFirstOperation = true;
 let isAnOperationClicked = false;
+let isOperationRecentlyClicked = false;
 
 buttonsContainer.addEventListener("click", isValidBtnInputForNumber);
 clearBtn.addEventListener("click", clearInputScreen);
 window.addEventListener("beforeunload", clearInputScreen);
-addBtn.addEventListener("click", handleAddition);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", handleOperation);
+subtractBtn.addEventListener("click", handleOperation);
+multiplyBtn.addEventListener("click", handleOperation);
+divideBtn.addEventListener("click", handleOperation);
 equalsBtn.addEventListener("click", handleResult);
+inputScreen.addEventListener("wheel", enableScreenVerticalScroll)
 
 
 function clearInputScreen() {
     inputScreen.value = "0";
     isFirstOperation = true;
+    isAnOperationClicked = false;
     num1 = num2 = result = 0;
 }
 
@@ -40,9 +43,9 @@ function isValidBtnInputForNumber(event) {
 
     if (event.target.id !== "dot" && isInputScreenInInitialState()) 
         inputScreen.value = "";
-    else if (isAnOperationClicked) {
+    else if (isOperationRecentlyClicked) {
         inputScreen.value = "";
-        isAnOperationClicked = false;
+        isOperationRecentlyClicked = false;
     }
         
     inputScreen.value += result[0];
@@ -51,22 +54,35 @@ function isValidBtnInputForNumber(event) {
 
 
 function handleResult() {
+    if (!isAnOperationClicked)
+        return;
+    
     num2 = +inputScreen.value;
 
     switch (operation) {
         case "addition":
             result = add(num1, num2);
-            console.log(result)
-            inputScreen.value = result;
-            num1 = num2 = 0;
+            break;
+        case "subtraction":
+            result = subtract(num1, num2);
+            break;
+        case "multiplication":
+            result = multiply(num1, num2);
+            break;
+        case "division":
+            result = divide(num1, num2);
             break;
         default:
             console.log("Invalid operation!");
     }
+
+    inputScreen.value = result;
+    num1 = num2 = 0;
+    isAnOperationClicked = false;
 }
 
 
-function handleAddition() {
+function handleOperation(event) {
     if (isAnOperationClicked)
         return;
 
@@ -76,9 +92,24 @@ function handleAddition() {
     } else {
         num1 = +inputScreen.value;
     }
-    operation = "addition";
+    isOperationRecentlyClicked = true; 
     isAnOperationClicked = true;
     isFirstOperation = false;
+
+    switch (event.target.id) {
+        case "add":
+            operation = "addition";
+            break;
+        case "subtract":
+            operation = "subtraction";
+            break;
+        case "multiply":
+            operation = "multiplication";
+            break;
+        case "divide":
+            operation = "division";
+            break;
+    }
 }
 
 
@@ -104,4 +135,10 @@ function divide(a, b) {
 
 function isInputScreenInInitialState() {
     return inputScreen.value === "0";
+}
+
+
+function enableScreenVerticalScroll(event) {
+    event.preventDefault();
+    event.target.scrollLeft += event.deltaY;
 }
