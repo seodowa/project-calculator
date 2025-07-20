20a45110b6ee9cd20fb3616067b50585be14e70f
seodowa <pagalan.theodoreustp@gmail.com>
2025-07-20 18:46:43 +0800

    Implement addition functionality

--- logic.js
+++ logic.js
@@ -1,18 +1,35 @@
 let buttonsContainer = document.querySelector(".input-buttons-container");
 let clearBtn = document.querySelector("#clear-empty");
 let inputScreen = document.querySelector("input");
+let addBtn = document.querySelector("#add");
+let subtractBtn = document.querySelector("#subtract");
+let multiplyBtn = document.querySelector("#multiply");
+let divideBtn = document.querySelector("#divide");
+let equalsBtn = document.querySelector("#equals");
 
-buttonsContainer.addEventListener("click", isValidInputForNumber);
+let num1 = num2 = result = null;
+let operation;
+let isFirstOperation = true;
+let isAnOperationClicked = false;
+
+buttonsContainer.addEventListener("click", isValidBtnInputForNumber);
 clearBtn.addEventListener("click", clearInputScreen);
 window.addEventListener("beforeunload", clearInputScreen);
+addBtn.addEventListener("click", handleAddition);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+equalsBtn.addEventListener("click", handleResult);
 
 
 function clearInputScreen() {
     inputScreen.value = "0";
+    isFirstOperation = true;
+    num1 = num2 = result = 0;
 }
 
 
-function isValidInputForNumber(event) {
+function isValidBtnInputForNumber(event) {
     let target = event.target;
     
     const re = RegExp("[\\d.]+");
@@ -21,10 +38,47 @@ function isValidInputForNumber(event) {
     if ((result = re.exec(target.textContent)) === null)
         return;
 
-    if (event.target.id !== "dot" && inputScreen.value === "0")
+    if (event.target.id !== "dot" && isInputScreenInInitialState()) 
         inputScreen.value = "";
-
+    else if (isAnOperationClicked) {
+        inputScreen.value = "";
+        isAnOperationClicked = false;
+    }
+        
     inputScreen.value += result[0];
+}
+
+
+
+function handleResult() {
+    num2 = +inputScreen.value;
+
+    switch (operation) {
+        case "addition":
+            result = add(num1, num2);
+            console.log(result)
+            inputScreen.value = result;
+            num1 = num2 = 0;
+            break;
+        default:
+            console.log("Invalid operation!");
+    }
+}
+
+
+function handleAddition() {
+    if (isAnOperationClicked)
+        return;
+
+    if (!isFirstOperation) {
+        handleResult();
+        num1 = result;
+    } else {
+        num1 = +inputScreen.value;
+    }
+    operation = "addition";
+    isAnOperationClicked = true;
+    isFirstOperation = false;
 }
 
 
@@ -45,4 +99,9 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a/b;
+}
+
+
+function isInputScreenInInitialState() {
+    return inputScreen.value === "0";
 }
